762741fae8acdc3cff230263e41feded5dfd877c
doomhammerhell <maiconrlyeh@gmail.com>
2020-11-26 19:45:42 -0300

    Adding the hook to handle the initial posts and ChakraUI

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import { Container, Flex, Spinner, VStack } from "@chakra-ui/core";
+import React, { useEffect, useState } from "react";
+import Post from "./components/post";
+import db from "./lib/firebase";
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // Hook to handle the initial fetching of posts
+
+    db.collection("posts")
+      .orderBy("createdAt", "desc")
+      .get()
+      .then((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setPosts(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <Container maxW="md" centerContent p={8}>
+        <VStack spacing={8} w="100%">
+          {posts.map((post) => (
+            <Post post={post} key={post.id} />
+          ))}
+        </VStack>
+      </Container>
+    </>
+  );
+};
+
+export default App;
